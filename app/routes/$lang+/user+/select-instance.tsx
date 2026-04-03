f7fabc68f88c9f8b58424e1e8e192fc688065ad6
Haroon Nasher <haroon_foad2003@yahoo.com>
2026-04-03 09:04:44 +0700

    #478 moved select instance business logic to the service layer

--- app/routes/$lang+/user+/select-instance.tsx
+++ app/routes/$lang+/user+/select-instance.tsx
@@ -1,150 +1,27 @@
 import { ActionFunctionArgs, useActionData, useNavigate, useNavigation, useSubmit } from "react-router";
 import { useEffect, useRef, useState } from "react";
-import { redirectDocument, useLoaderData } from "react-router";
+import { useLoaderData } from "react-router";
 import { LoaderFunctionArgs } from "react-router";
 
 import {
-	getCountryAccountsIdFromSession,
-	getUserFromSession,
-	sessionCookie,
-} from "~/utils/session";
-import { getSafeRedirectTo } from "./login";
-import { UserCountryAccountRepository } from "~/db/queries/userCountryAccountsRepository";
-import { CountryAccountsRepository } from "~/db/queries/countryAccountsRepository";
-import { CountryRepository } from "~/db/queries/countriesRepository";
-import { SelectUserCountryAccounts } from "~/drizzle/schema/userCountryAccountsTable";
-import {
-	countryAccountStatuses,
 	countryAccountTypesTable,
-	SelectCountryAccounts,
 } from "~/drizzle/schema/countryAccountsTable";
-import { SelectCountries } from "~/drizzle/schema/countriesTable";
-import { InstanceSystemSettingRepository } from "~/db/queries/instanceSystemSettingRepository";
-import { redirectLangFromRoute, replaceLang } from "~/utils/url.backend";
+import { LoaderDataType, SelectInstanceService } from "~/services/selectInstanceService";
 
 import { ViewContext } from "~/frontend/context";
 
-import { BackendContext } from "~/backend.server/context";
 import { Toast } from "primereact/toast";
 import { ListBox } from "primereact/listbox";
 import { Dialog } from "primereact/dialog";
 import Tag from "~/components/Tag";
 import { Button } from "primereact/button";
 
-type LoaderDataType = SelectUserCountryAccounts & {
-	countryAccount: Partial<SelectCountryAccounts> & {
-		country: Partial<SelectCountries>;
-	};
-};
-
 export const loader = async (args: LoaderFunctionArgs) => {
-	const { request } = args;
-	const ctx = new BackendContext(args);
-
-	const userSession = await getUserFromSession(request);
-	if (!userSession) {
-		return redirectLangFromRoute(args, "/user/login");
-	}
-
-	const url = new URL(request.url);
-	let cancelRedirectTo = url.searchParams.get("redirectTo") || "/";
-	if (cancelRedirectTo === "/") {
-		cancelRedirectTo = ctx.url("/hazardous-event/");
-	}
-
-	cancelRedirectTo = getSafeRedirectTo(ctx, cancelRedirectTo);
-
-	const countryAccountIdFromSession = await getCountryAccountsIdFromSession(request);
-
-	const userCountryAccounts = await UserCountryAccountRepository.getByUserId(
-		userSession.user.id,
-	);
-
-	if (!userCountryAccounts || userCountryAccounts.length === 0) {
-		return redirectLangFromRoute(args, "/user/login");
-	}
-
-	const data: LoaderDataType[] = (
-		await Promise.all(
-			userCountryAccounts.map(async (uca) => {
-				if (!uca.countryAccountsId) return;
-				const countryAccount = await CountryAccountsRepository.getById(
-					uca.countryAccountsId,
-				);
-				if (
-					!countryAccount ||
-					countryAccount.status !== countryAccountStatuses.ACTIVE
-				) {
-					return null;
-				}
-
-				const country = await CountryRepository.getById(countryAccount.countryId);
-				if (!country) return null;
-
-				return {
-					...uca,
-					countryAccount: {
-						...countryAccount,
-						country,
-					},
-				};
-			}),
-		)
-	).filter(Boolean) as LoaderDataType[];
-
-	// Sort by country name
-	data.sort((a, b) => {
-		const nameA = a.countryAccount.country.name || "";
-		const nameB = b.countryAccount.country.name || "";
-		return nameA.localeCompare(nameB);
-	});
-
-	return {
-		data,
-		hasSessionCountryAccountId: Boolean(countryAccountIdFromSession),
-		cancelRedirectTo,
-	};
+	return SelectInstanceService.loader(args);
 };
 
 export const action = async (args: ActionFunctionArgs) => {
-	const { request } = args;
-	const formData = await request.formData();
-	const countryAccountsId = formData.get("countryAccountsId");
-	const ctx = new BackendContext(args);
-
-	const errors: Record<string, string> = {};
-	if (!countryAccountsId || typeof countryAccountsId !== "string") {
-		errors.countryInstance = "Select an instance first";
-		return {
-			ok: false,
-			errors
-		}
-	}
-
-	const url = new URL(request.url);
-	let redirectTo = url.searchParams.get("redirectTo") || "/";
-	if (redirectTo === "/") {
-		redirectTo = ctx.url("/hazardous-event/");
-	}
-
-	redirectTo = getSafeRedirectTo(ctx, redirectTo);
-
-	const session = await sessionCookie().getSession(
-		request.headers.get("Cookie"),
-	);
-
-	const countrySettings =
-		await InstanceSystemSettingRepository.getByCountryAccountId(countryAccountsId);
-
-	session.set("countryAccountsId", countryAccountsId);
-	session.set("countrySettings", countrySettings);
-	const setCookie = await sessionCookie().commitSession(session);
-
-	redirectTo = replaceLang(redirectTo, countrySettings?.language || "en");
-
-	return redirectDocument(redirectTo, {
-		headers: { "Set-Cookie": setCookie },
-	});
+	return SelectInstanceService.action(args);
 };
 
 export default function SelectInstance() {
